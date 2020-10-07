d61e4c5ba2c7da28334e235553b2d17748eab6a0
derwolz <32439939+derwolz@users.noreply.github.com>
2020-10-07 17:58:26 -0600

    Update main.cpp
    Add Comments, Delete dimensions pointer

--- ImageSplicer/main.cpp
+++ ImageSplicer/main.cpp
@@ -5,8 +5,10 @@ using namespace std;
 using namespace sf;
 int main()
 {
-
+    //array holding original image dimensions and destination sprite dimensions
     int* dimensions = new int[4];
+    
+    /* Basic Input Questions */
     std::string filename;
     cout << "Enter image name";
     cin >> filename;
@@ -18,17 +20,26 @@ int main()
     cin >> dimensions[2];
     cout << "Enter Image height";
     cin >> dimensions[3];
+    
+    //Calculate number of files
     int numfiles = dimensions[2] / dimensions[0] * dimensions[3] / dimensions[1];
+    
+    //Generate Filepath string
     string filepath;
     filepath = "Input/" + filename + ".png";
     string outputpath;
+    
+    //end path to place files
     outputpath = "Output/";
+    
     Image image;
     image.loadFromFile(filepath);
     Image* output = new Image[numfiles];
     int left = numfiles * dimensions[1] / dimensions[3];
     int top = numfiles * dimensions[0] / dimensions[2];
     int countsuccess = 0;
+    
+    //Slice the image into pieces, save to file location as filename + Number
     for (int j = 0; j < top; j++) {
         for (int i = 0; i < left; i++) {
             char buffer[25];
@@ -41,8 +52,12 @@ int main()
             countsuccess += output[i].saveToFile(savelloc);
         }
     }
+    //Print Total vs successes
     cout << numfiles << " Attempted. " << countsuccess << " Succeeded" << endl;
+    //destroy all pointers
     delete[] output;
+    delete[] dimensions;
+    dimensions = nullptr;
     output = nullptr;
     return 0;
 }
